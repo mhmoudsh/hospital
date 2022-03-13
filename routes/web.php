d080a86e3f5d68eb00d05c224bfb68bf03c6a5e0
mhmoudsh <mmh1999.2014@gmail.com>
2022-03-13 19:06:27 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\DepartmentController;
+
+Route::prefix('admin')->name('admin.')->middleware('auth', 'check_type')->group(function() {
+    Route::get('/', [AdminController::class, 'index'])->name('index');
+
+    // admin.departments.index
+    // admin.departments.create
+    // admin.departments.store
+    // admin.departments.edit
+    // admin.departments.update
+    // admin.departments.destroy
+    // admin.departments.show
+    Route::resource('departments', DepartmentController::class);
+
+});
+
+
+Route::get('/', function() {
+
+    // return view('welcome');
+})->name('homepage');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::view('/not-allowed', 'not_allowed');
